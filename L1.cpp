bb78a0fe61571e2675732a73b14bbe1fad4da7fd
Chetanya <chetanyaarora111@gmail.com>
2026-07-12 23:21:33 +0530

    BruteForce

---
+++ L1.cpp
@@ -0,0 +1,34 @@
+/*Given an array of integers "nums" and an integer "target", return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.*/
+
+// Bruteforce: TC - O(N2), SC: O(1)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    int size = nums.size();
+    for(int i = 0; i < size-1; i++) {
+        for(int j = i+1; j < size; j++) {
+            if(nums[i] + nums[j] == target) {
+                return {i,j};
+            }
+        }
+    }
+    return {};
+}
+
+void display(vector<int>& indices) {
+    for (int i = 0; i < indices.size(); i++) {
+        cout << indices[i] << "\t";
+    }
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4,5};
+    int target = 6;
+    vector<int> indices = twoSum(nums, target);
+    display(indices);
+    return 0;
+}
