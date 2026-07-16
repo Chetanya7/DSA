4fa36254f76d88ed9fc0ccd047c7eb6a91ff617c
Chetanya <chetanyaarora111@gmail.com>
2026-07-17 00:26:41 +0530

    CF-1899A

---
+++ CP/1899A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        if(n%3 == 0){
+            cout << "Second" << endl;
+        } else {
+            cout << "First" << endl;
+        }
+    }
+}
