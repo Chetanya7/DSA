519b82a43545d4a8fcdb6cf5e1c466f4708d3e52
Chetanya <chetanyaarora111@gmail.com>
2026-07-16 15:18:54 +0530

    L169-better

--- LC/L169.cpp
+++ LC/L169.cpp
@@ -5,7 +5,8 @@ You may assume that the majority element always exists in the array.
 */
 
 //Bruteforce: TC: O(N2), SC: O(1)
-#include <iostream>
+
+/*#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -19,6 +20,19 @@ int majorityElement(vector<int>& nums) {
         if(count > threshold) return nums[i];
     }
     return -1;
+}*/
+
+// Better
+// TC: O(nlogn) as sort which uses Introsort (an hybrid of Heapsort, Quicksort and insertion sort) takes O(nlogn) time
+// SC: O(logn) - Even thought the sorting is in-place, that is because Introsort is recursive and requires O(logn) auxiliary space on the stack.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int majorityElement(vector<int>& nums){
+    sort(nums.begin(), nums.end());
+    return nums[nums.size()/2];
 }
 
 int main() {
