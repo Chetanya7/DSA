2982d3a29ef9c1445d4adde9233a01b933b37965
Chetanya <chetanyaarora111@gmail.com>
2026-07-23 20:35:25 +0530

    L724-better

--- LC/L724.cpp
+++ LC/L724.cpp
@@ -10,30 +10,53 @@ because there are no elements to the left. This also applies to the right edge o
 Return the leftmost pivot index. If no such index exists, return -1.
 */
 
-// Optimal
+// Better: using prefix sum
 // TC: O(n)
-// SC: O(1)
-
+// SC: O(n)
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int pivotIndex(vector<int>& nums) {
-    int sum = 0;
-    for(int num : nums) {
-        sum += num;
+    int n = nums.size();
+    vector<int> prefix(n+1,0);
+    for(int i=0; i<n; i++) {
+        prefix[i+1] = prefix[i] + nums[i];
     }
-    int lSum = 0;
-    int rSum = sum - nums[0];
 
-    for(int i=0; i<nums.size(); i++) {
+    for(int i=0; i<n; i++) {
+        int lSum = prefix[i];
+        int rSum = prefix[n] - lSum - nums[i];
         if(lSum == rSum) return i;
-        if(i != nums.size()-1) {
-            lSum += nums[i];
-            rSum -= nums[i+1];
-        }
-
     }
     return -1;
 }
+
+// Optimal
+// TC: O(n)
+// SC: O(1)
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int pivotIndex(vector<int>& nums) {
+//     int sum = 0;
+//     for(int num : nums) {
+//         sum += num;
+//     }
+//     int lSum = 0;
+//     int rSum = sum - nums[0];
+
+//     for(int i=0; i<nums.size(); i++) {
+//         if(lSum == rSum) return i;
+//         if(i != nums.size()-1) {
+//             lSum += nums[i];
+//             rSum -= nums[i+1];
+//         }
+
+//     }
+//     return -1;
+// }
