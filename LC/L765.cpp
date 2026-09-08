ed0df05261ec73466b2d795d4a1db9e7cb5262c6
Chetanya <chetanyaarora111@gmail.com>
2026-09-08 18:29:58 +0530

    L765{Greedy}

---
+++ LC/L765.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int minSwapCouples(vector<int> row){
+    int n = row.size();
+    int swaps = 0;
+
+    for(int i=0; i<n; i=i+2){
+        int person = row[i];
+        int partner = person ^ 1; // find who is the partner of the person
+
+        // find where the partner is seated
+        int j=i+1;
+        while(row[j]!=partner){
+            j++;
+        }
+
+        if(j != (i+1)){
+            row[j] = row[i+1];
+            row[i+1] = partner;
+            swaps++;
+        }
+    }
+
+    return swaps;
+}
+
+int main(){
+    vector<int> row = {3,2,0,1};
+    int swaps = minSwapCouples(row);
+    cout << swaps;
+    return 0;
+}
